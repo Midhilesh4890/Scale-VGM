80caef9cb0e5d3af969d7dca4bb3f251227ddb49
midhilesh <chintu.midhilesh@gmail.com>
2024-11-16 07:54:57 +0530

    Added distributed dask code for parallel processing

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-import pandas as pd 
-df = pd.read_csv('data/scaled_data_10m.csv')
-print(df.columns)
-print(df.shape)
+# import pandas as pd 
+# df = pd.read_csv('data/scaled_data_10m.csv')
+# print(df.columns)
+# print(df.shape)
+
+
+import dask.dataframe as dd
+# Path to the partitioned Parquet directory
+parquet_path = "data/scaled_data_1b/"
+
+# Read all partitioned Parquet files
+df = dd.read_parquet(parquet_path)
+
+# Perform operations or convert to Pandas
+print(df.head())  # View the first few rows
+print(f"Total rows: {df.shape[0].compute()}")  # Compute total rows
